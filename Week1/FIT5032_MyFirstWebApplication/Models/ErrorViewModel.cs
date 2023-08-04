7c3f95f82d2d2654ee6fe7bbc94108758cc80081
Bao Hoang <ghoa0004@student.monash.edu>
2023-08-05 02:08:09 +1000

    Add project files.

---
+++ Week1/FIT5032_MyFirstWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FIT5032_MyFirstWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
